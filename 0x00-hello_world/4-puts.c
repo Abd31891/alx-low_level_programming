5fcde189641cb75748424db0fa39a0adf46b5730
abdullah <aa3630434@gmail.com>
2023-08-01 06:37:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.n>
+/**
+ * main - A program that prints a line with pfunction
+ * return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
+
+
