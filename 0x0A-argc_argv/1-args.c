970e6f7d8ea688e287e547c6221d787366ff4556
abdullah <aa3630434@gmail.com>
2023-08-05 03:03:41 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,15 +2,17 @@
 
 
 /***
- * main - print number of arguments passed. 
- * @argc: number of command line arguments.
- * @argv: pointer to an array of command line argument.  
- * Return: 0-success, non-zero-fail.
+ * main-print number of arguments passed.
+ *
+ * @argc:number of command line arguments.
+ * @argv:pointer to an array of command line argument.
+ *
+ * Return:0-success, non-zero-fail.
  */
 
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	     print("%d\n",argc - 1);
-	     return (0);
+	     print("%d\n", argc - 1);
+	     Return(0);
 }
