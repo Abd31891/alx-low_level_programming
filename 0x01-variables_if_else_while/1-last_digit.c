ee2e7d99421eb8dfaf560a7ab66e110ad94de3f3
abdullah <aa3630434@gmail.com>
2023-08-05 01:53:23 -0700

    0-whatsmyname.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Discription: a
+ *
+ * Return: 0 (Success)
+ * */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
+}
